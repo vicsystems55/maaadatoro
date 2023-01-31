4c8c635057bda5bd2328c66ea8231292ec66c797
vicSystems <victorasuquob@gmail.com>
2023-01-31 15:01:08 +0100

    site setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -396,65 +396,7 @@
       </div>
     </section>
     <!-- End Team Section -->
-    <hr>
-    <!-- Start home-blog -->
-    <section class="home-blog section" id="blog">
-      <div class="container">
-        <div class="section-header type1">
-          <h2>Don't Miss Latest News</h2>
-          <div class="sub-heading">We post every week about the construction-related thing you can<br> 
-          read on our site and update every construction news.</div>
-        </div>
-        <div class="row flex">
-          <div class="col-lg-4">
-            <article class="post">
-              <div class="zoom-effect">
-                <a href="blog-details-right-sidebar.html" class="post-thumbnail zoom-effect-in"><img src="img/blog/03.jpg" alt="Image"></a>
-              </div>
-              <div class="post-details-wrap">
-                <div class="byline">
-                  <span class="author"><a href="#">Admin</a></span>
-                  <span class="comment"><a href="#">3</a></span>
-                  <span class="posted-on"><a href="#">10 Jan</a></span>
-                </div>
-                <h2 class="entry-title"><a href="blog-details-right-sidebar.html">How to construct beautiful houses as you were very cheap to build.</a></h2>
-              </div>
-            </article>
-          </div><!-- .col -->
-          <div class="col-lg-4">
-            <article class="post">
-              <div class="zoom-effect">
-                <a href="blog-details-right-sidebar.html" class="post-thumbnail zoom-effect-in"><img src="img/blog/04.jpg" alt="Image"></a>
-              </div>
-              <div class="post-details-wrap">
-                <div class="byline">
-                  <span class="author"><a href="#">Admin</a></span>
-                  <span class="comment"><a href="#">3</a></span>
-                  <span class="posted-on"><a href="#">14 Feb</a></span>
-                </div>
-                <h2 class="entry-title"><a href="blog-details-right-sidebar.html">Built your beautiful houses with latest technology which we use to build.</a></h2>
-              </div>
-            </article>
-          </div><!-- .col -->
-          <div class="col-lg-4">
-            <article class="post">
-              <div class="zoom-effect">
-                <a href="blog-details-right-sidebar.html" class="post-thumbnail zoom-effect-in"><img src="img/blog/05.jpg" alt="Image"></a>
-              </div>
-              <div class="post-details-wrap">
-                <div class="byline">
-                  <span class="author"><a href="#">Admin</a></span>
-                  <span class="comment"><a href="#">3</a></span>
-                  <span class="posted-on"><a href="#">20 Apr</a></span>
-                </div>
-                <h2 class="entry-title"><a href="blog-details-right-sidebar.html">The Best Architecture Projects of the 21st Century is this.</a></h2>
-              </div>
-            </article>
-          </div><!-- .col -->
-        </div>
-      </div>
-    </section>
-    <!-- End home-blog -->
+ 
 
     <!-- Start Testimonial -->
     <section class="testimonial-wrap section gray-bg" id="testimonial">
